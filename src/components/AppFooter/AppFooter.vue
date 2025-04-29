9db2caea0b890fe88aba773e218fdb2f9e6eae90
gabrielbsb21 <gabrielbsb21@outlook.com>
2025-04-29 10:53:35 -0300

    work in progress

---
+++ src/components/AppFooter/AppFooter.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import styles from './footer.module.css'
+</script>
+
+<template>
+  <footer :class="styles.footer">
+    <section>footer</section>
+  </footer>
+</template>
---
+++ src/components/AppHeader/AppHeader.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import styles from './header.module.css'
+</script>
+
+<template>
+  <header :class="styles.header">
+    <div class="container-xl">
+      <h1 class="visuallyhidden">Nome do Casal</h1>
+      <RouterLink to="/">
+        <img src="" alt="Logo do casal" />
+      </RouterLink>
+      <nav aria-label="navegação principal">
+        <ul>
+          <li>
+            <RouterLink to="/">Início</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/presentes">Presentes</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/contato">Contato</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/sobre">Sobre</RouterLink>
+          </li>
+        </ul>
+      </nav>
+      <button>Trocar tema</button>
+    </div>
+  </header>
+</template>
